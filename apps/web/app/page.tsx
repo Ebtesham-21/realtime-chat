d22b9b51e746a8d2d7d380ebc93876191de000de
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-05-15 18:38:10 +0600

    redis issue

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,25 @@
-'use client'
+'use client';
+import {useSocket} from '../context/SocketProvider';
+import { useState } from 'react';
+import classes from './page.module.css';
+
+
 
 export default function Page() {
+  const { sendMessage } = useSocket();
+  const [message, setMessage] = useState('');
+
+
    return (
     <div>
       <div>
-        All Messages will be shown here
+          <h1> All Messages will be shown here </h1>
       </div>
       <div>
-        <input type="text" placeholder="Type your message here" />
-        <button>Send</button>
+        <input onChange = {e => setMessage(e.target.value)} className={classes["chat-input"]} placeholder="Type your message here" />
+        <button onClick={e => sendMessage(message)} className={classes["button"]} >Send</button>
       </div>
 
     </div>
-   )
+   );
 }
--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -1,6 +1,6 @@
 'use client';
-import React, { useCallback, useContext, useEffect } from "react";
-import {io} from 'socket.io-client';
+import React, { useCallback, useState, useContext, useEffect } from "react";
+import {io, Socket} from 'socket.io-client';
 
 
 interface SocketProviderProps {
@@ -24,18 +24,25 @@ export const useSocket = () => {
 }
 
 export const SocketProvider: React.FC<SocketProviderProps> = ({children}) => {
-
+    const [socket, setSocket] = useState<Socket>();
     const sendMessage: ISocketContext['sendMessage'] = useCallback((msg) => {
         console.log("Send Message", msg);
+
+        if (socket) {
+            Socket.emit('event:message', { message: msg });
+        }
         
-    }, []);
+    }, [socket]);
 
 
     useEffect(() => {
         const _socket = io('http://localhost:8000')
+        setSocket(_socket);
+
 
         return () => {
            _socket.disconnect(); 
+           setSocket(undefined);
         };
     }, []);
 
