9099e6664bec3f324c0b9a64ee2f58d3dbc0e1aa
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-05-15 12:24:25 +0600

    socket ts file issue

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,11 +1,18 @@
-import http from 'http'
+import http from 'http';
+import SocketService from './services/socket';      
+import socket from './services/socket';
+
 async function init() {
+    const socketService = new SocketService();
     const httpServer = http.createServer();
     const PORT = process.env.PORT ? process.env.PORT : 8000;
+    socketService.io.attach(httpServer);
 
     httpServer.listen(PORT, () => {
         console.log(`HTTP Server is running on port ${PORT}`);
     });
+
+    socketService.initListeners();
 
 }
 
--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,18 +1,32 @@
 import { Server } from "socket.io";
 
 class SocketService {
-    
+    private _io:Server;
 
     constructor(){
         console.log("Init Socket Service...");
-        this._io = new Server(
-
-        )
-
-        get io() {
-            return this._io;
-        }
+        this._io = new Server()
     }
-}
 
-export default SocketService;
+    public initListeners() {
+        const io = this._io;
+        console.log("Init Socket Listeners...");
+        io.on('connect',  socket => {
+            console.log(`New Socket Connected`, socket.id);
+
+            socket.on('event:message', async ({message} : {message: string} ) => {
+                console.log("New Message Rec.", message);
+            } );
+        } );
+
+    }
+
+    get io() {
+            return this._io;
+        } 
+    
+
+    }
+
+    
+export default   SocketService();
