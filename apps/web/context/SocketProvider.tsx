bcb679fd8900091df9d279042d38f7367e4c1366
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-05-15 16:46:29 +0600

    front end fetch issue

--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -1,5 +1,7 @@
-'use client'
-import React, { useCallback } from "react"
+'use client';
+import React, { useCallback, useContext, useEffect } from "react";
+import {io} from 'socket.io-client';
+
 
 interface SocketProviderProps {
     children?: React.ReactNode
@@ -12,13 +14,31 @@ interface ISocketContext {
 
 const SocketContext = React.createContext<ISocketContext | null>(null);
 
+
+export const useSocket = () => {
+     const state = useContext(SocketContext)
+     if(!state) throw new Error(`state is undefined`)
+
+     return state;
+
+}
+
 export const SocketProvider: React.FC<SocketProviderProps> = ({children}) => {
 
     const sendMessage: ISocketContext['sendMessage'] = useCallback((msg) => {
         console.log("Send Message", msg);
         
     }, []);
-    
+
+
+    useEffect(() => {
+        const _socket = io('http://localhost:8000')
+
+        return () => {
+           _socket.disconnect(); 
+        };
+    }, []);
+
     return (
         <SocketContext.Provider value={null}>
             {children}
